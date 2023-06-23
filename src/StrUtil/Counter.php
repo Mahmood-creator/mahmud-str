c49a77055445ed0de1e29c90a487efe885e54f95
Mahmud <mamatovmahmud1999@gmail.com>
2023-06-23 20:56:37 +0500

    Initial commit

---
+++ src/StrUtil/Counter.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Created by Pangodream.
+ * Date: 14/04/2019
+ * Time: 18:50
+ */
+
+namespace StrUtil;
+
+
+class Counter
+{
+    /**
+     * @param string $text The text we want to count the number of words it consist of
+     * @return string The number of words in text
+     */
+    public static function text($text)
+    {
+        return $text;
+    }
+}
